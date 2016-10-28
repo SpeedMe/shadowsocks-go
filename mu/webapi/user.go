d75e1ad7aa1621f0dd08f850e4f813fe2d6cf4f7
huanglei <huanglei@zhubajie.com>
2016-10-29 02:40:46 +0800

    节点配置

--- mu/webapi/user.go
+++ mu/webapi/user.go
@@ -40,6 +40,9 @@ func (u User) IsEnable() bool {
 		return false
 	}
 	for _, node := range strings.Split(u.NodeIds, "#") {
+		Log.Info(fmt.Sprintf("available server nodes %v ...\n", u.NodeIds))
+		Log.Info(fmt.Sprintf("available server node %v ...\n", node))
+		Log.Info(fmt.Sprintf("server node %v ...\n", client.nodeId))
         	if strings.EqualFold(strconv.Itoa(client.nodeId), node) {
 			return true
 		}
